baee8f08aa710cc839d49ccd0b9d241773d198db
Niko <contact@3desquisse.fr>
2017-12-21 11:44:14 +0100

    Add ObserveBirdLocation (form and validation)

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -22,52 +22,52 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('mail', EmailType::class, [
-            'mapped' => false,
-            'constraints' => [
-                new NotBlank([
-                    'message' => "Vous devez saisir un mail"
-                ]),
-                new Email([
-                    'message' => "L’email {{ value }} n’est pas un mail valide",
-                    'checkMX' => true
-                ])
-            ]
-        ])
-        ->add('userName', TextType::class, [
-            'mapped' => false,
-            'constraints' => [
-                new NotBlank([
-                    'message' => "Vous devez saisir un nom"
-                ]),
-                new Type([
-                    'type' => 'string',
-                    'message' => "Vous devez saisir une chaine de caractère"
-                ]),
-                new Length([
-                    'min' => 5,
-                    'max' => 50,
-                    'minMessage' => "Vous devez entrer au moins {{ limit }} caractères",
-                    'maxMessage' => "Vous devez entrer moins de {{ limit }} caractères"
-                ])
-            ]
-        ])
-        ->add('message', TextareaType::class, [
-            'mapped' => false,
-            'constraints' => [
-                new NotBlank([
-                    'message' => "Vous devez saisir un message"
-                ]),
-                new Type([
-                    'type'    => 'string',
-                    'message' => "Vous devez saisir une chaine de caractère"
-                ]),
-                new Length([
-                    'max' => 255,
-                    'maxMessage' => "Vous devez entrer moins de {{ limit }} caractères"
-                ])
-            ]
-        ])
+            ->add('mail', EmailType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Vous devez saisir un mail"
+                    ]),
+                    new Email([
+                        'message' => "L’email {{ value }} n’est pas un mail valide",
+                        'checkMX' => true
+                    ])
+                ]
+            ])
+            ->add('userName', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Vous devez saisir un nom"
+                    ]),
+                    new Type([
+                        'type' => 'string',
+                        'message' => "Vous devez saisir une chaine de caractère"
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'max' => 50,
+                        'minMessage' => "Vous devez entrer au moins {{ limit }} caractères",
+                        'maxMessage' => "Vous devez entrer moins de {{ limit }} caractères"
+                    ])
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Vous devez saisir un message"
+                    ]),
+                    new Type([
+                        'type'    => 'string',
+                        'message' => "Vous devez saisir une chaine de caractère"
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => "Vous devez entrer moins de {{ limit }} caractères"
+                    ])
+                ]
+            ])
         ;
     }
 

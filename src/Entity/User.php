7befbd4459fd42499aa6ee286d580256ef741f61
Niko <contact@3desquisse.fr>
2018-01-23 21:29:44 +0100

    Change for tunnel functionnality

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -107,7 +107,7 @@ class User implements UserInterface, AdvancedUserInterface
     private $picture;
 
     /**
-     * 
+     *
      * @ORM\Column(name="is_active", type="boolean")
      */
     private $isActive;
@@ -149,7 +149,7 @@ class User implements UserInterface, AdvancedUserInterface
 
 
     //GETTERS SETTERS
-    
+
     /**
      * @return mixed
      */
@@ -268,6 +268,27 @@ class User implements UserInterface, AdvancedUserInterface
         $this->roles = $roles;
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFunction()
+    {
+
+        $function = array_map(function($role) {
+            if (in_array($role, ['ROLE_ADMIN'])) {
+                return 'Administrateur';
+            }
+            if (in_array($role, ['ROLE_NATURALIST'])) {
+                return 'Naturaliste';
+            }
+            if (in_array($role, ['ROLE_USER'])) {
+                return 'Membre';
+            }
+        },$this->roles);
+
+        return $function[0];
     }
 
     /**

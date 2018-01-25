4c890c54b06650950653c0816e1f9118ef2e2e75
Niko <contact@3desquisse.fr>
2018-01-25 17:40:23 +0100

    minor change for integration

--- src/Controller/TaxrefController.php
+++ src/Controller/TaxrefController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/taxref")

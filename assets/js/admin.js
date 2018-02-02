83fe1ca658006f325defa78458eedd89419f8017
Niko <contact@3desquisse.fr>
2018-02-02 09:40:26 +0100

    Fix bug admin Obs modal window

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,6 @@
 // admin.js
 
+import 'bootstrap';
 import './datepicker';
 
 $(function() {
@@ -59,14 +60,13 @@ $(document).on('submit', 'form[data-confirmationDelete]', function (event) {
   var $form = $(this),
       $confirm = $('#deleteConfirmationModal');
 
-  if ($confirm.data('result') !== 'yes') {
-      //cancel submit event
-      event.preventDefault();
-      // console.log($confirm.modal('click', '#btnYes'))
+      if ($confirm.data('result') !== 'yes') {
+        //cancel submit event
+        event.preventDefault();
 
-      $confirm
-          .off('click', '#btnYes')
-          .on('click', '#btnYes', function () {
+        $confirm
+        .off('click', '#btnYes')
+        .on('click', '#btnYes', function () {
               $confirm.data('result', 'yes');
               $form.find('input[type="submit"]').attr('disabled', 'disabled');
               $form.submit();
